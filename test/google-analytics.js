d6482eba5df6b2a09b1129f92344c60251835159
Gibran Malheiros <gibatronic@gmail.com>
2014-12-15 15:27:15 -0200

    refactored test to reuse some code and keep things DRY

--- test/google-analytics.js
+++ test/google-analytics.js
@@ -1,42 +1,45 @@
 describe('google analytics', function() {
-  it('should throw an error when account is missing', function() {
-    var wrapper = function() {
-      new GoogleAnalytics({
-        category: 'category'
-      });
-    };
+  var factory = function(options) {
+    options || (options = {
+      account: 'account',
+      category: 'category'
+    });
 
-    expect(wrapper).toThrow();
+    return new GoogleAnalytics(options);
+  };
+
+  factory.with = function(options) {
+    return factory.bind(window, options);
+  };
+
+  it('should throw an error when account is missing', function() {
+    expect(factory.with({
+      category: 'category'
+    })).toThrow();
   });
 
   it('should throw an error when category is missing', function() {
-    var wrapper = function() {
-      new GoogleAnalytics({
-        account: 'account'
-      });
-    };
-
-    expect(wrapper).toThrow();
+    expect(factory.with({
+      account: 'account'
+    })).toThrow();
   });
 
   it('should not throw an error with required options present', function() {
-    var wrapper = function() {
-      new GoogleAnalytics({
-        account: 'account',
-        category: 'category'
-      });
-    };
+    expect(factory).not.toThrow();
+  });
 
-    expect(wrapper).not.toThrow();
+  it('should declare the global _gaq queue', function() {
+    delete window._gaq;
+
+    factory();
+
+    expect(window._gaq).not.toBeUndefined();
   });
 
   it('should inject the SDK script', function() {
     var scripts;
 
-    new GoogleAnalytics({
-      account: 'account',
-      category: 'category'
-    });
+    factory();
 
     scripts = document.querySelectorAll('#google-analytics-sdk');
 
