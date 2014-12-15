0ecf880bd9b9613d93ab2287e9a7de6ecfc3d254
Gibran Malheiros <gibatronic@gmail.com>
2014-12-15 15:22:47 -0200

    added a polyfill for Function.prototype.bind
    PhantonJS uses an old version of javascript core:
    https://github.com/ariya/phantomjs/issues/10522

--- karma.js
+++ karma.js
@@ -7,6 +7,7 @@ module.exports = function(config) {
     ],
     colors: true,
     files: [
+      'node_modules/polyfill-function-prototype-bind/bind.js',
       'build/*.js',
       'test/*.js'
     ],
